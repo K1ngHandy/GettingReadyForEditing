7a1f870a0e42eb626b869d337d80a11ccb0c9961
Steve H <a.king.life@pm.me>
2023-03-12 13:50:34 -0400

    create edit button

--- StartingProject/Today/DetailViewController/ReminderViewController.swift
+++ StartingProject/Today/DetailViewController/ReminderViewController.swift
@@ -36,8 +36,18 @@ class ReminderViewController: UICollectionViewController {
             navigationItem.style = .navigator
         }
         navigationItem.title = NSLocalizedString("Reminder", comment: "Reminder view controller title")
+        navigationItem.rightBarButtonItem = editButtonItem
 
-                updateSnapshotForViewing()
+        updateSnapshotForViewing()
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        if editing {
+            updateSnapshotForEditing()
+        } else {
+            updateSnapshotForViewing()
+        }
     }
 
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
